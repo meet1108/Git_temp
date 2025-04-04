53470762641fb79f58ea66b1d28916a1bd4e6516
Meet <meet11@gmail.com>
2025-04-04 11:55:38 +0530

    add Multiply.java

---
+++ multiply.java
@@ -0,0 +1,13 @@
+public class multiply {
+    
+   
+        public static void main(String args[]){
+            int a,b;
+             a = 5;
+            b= 6;
+    
+            System.out.println("ans = " + (a*b));
+    
+        }
+    
+}
