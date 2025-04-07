7ce4fa64df87fe543c6af80ce3596b5d727ea83b
Meet <meet11@gmail.com>
2025-04-07 12:32:22 +0530

    new branch without merge

---
+++ demo.java
@@ -0,0 +1,11 @@
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("New Branch");
+        int a,b;
+         a = 5;
+        b= 6;
+
+        System.out.println("sum = " + (a+b));
+
+    }
+}
